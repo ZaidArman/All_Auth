de9b295bd3d2b0977cdcc8544cd86992778815e3
Zaid <zahidu864@gmai.com>
2024-04-04 09:12:41 +0500

    Django Social AllAuth

--- All_Auth/urls.py
+++ All_Auth/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView # use generic views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='dashboard/home.html'), name='home'),
     path('login', TemplateView.as_view(template_name='account/login.html'), name='login'),
     path('accounts/', include('allauth.urls')),
-    
 ]
