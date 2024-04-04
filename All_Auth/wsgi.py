1887e62fd0342253e915ad1cb1eee8c2d2ef9ec3
Zaid <zahidu864@gmai.com>
2024-04-04 09:24:33 +0500

    Changes

--- All_Auth/wsgi.py
+++ All_Auth/wsgi.py
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-"""
-WSGI config for All_Auth project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -15,21 +5,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "All_Auth.settings")
 
 application = get_wsgi_application()
-=======
-"""
-WSGI config for All_Auth project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "All_Auth.settings")
-
-application = get_wsgi_application()
->>>>>>> eaee593af9b42438a0d4098d3e1fe016e59010e4
